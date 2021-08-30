3de3aa3e4287dd7e4ea2463347dcd6982623455c
Aashish <aashishsharma.ajm@gmail.com>
2021-08-30 20:41:09 +0530

    optimised the findMinimum.py code with more clever implementation

--- Stack/findMinimum.py
+++ Stack/findMinimum.py
@@ -5,11 +5,31 @@ class MinStack:
         self.size = size
         self.stk = []
         self.minStk = []
-        self.top = -1
+        self.brim = -1
     
-    def isEmpty(self): return (self.top == -1)
-    def isFull(self): return (self.top == self.size)
+    def isEmpty(self): return (self.brim == -1)
+    def isFull(self): return (self.brim == self.size)
 
+
+    '''
+        This code is not optimized for space therefore i will make one that is
+
+        def push(self, data):
+            if(self.isFull()):
+                raise Exception("Stack is full!")
+            else:
+                if(self.isEmpty()):
+                    self.minStk.append(data)
+                else:
+                    if(self.minStk[-1] <= data):
+                        self.minStk.append(self.minStk[-1])
+                    else:
+                        self.minStk.append(data)
+                self.stk.append(data)
+                self.brim+=1
+    '''
+
+    #This code is optimized for space
 
     def push(self, data):
         if(self.isFull()):
@@ -18,20 +38,29 @@ class MinStack:
             if(self.isEmpty()):
                 self.minStk.append(data)
             else:
-                if(self.minStk[-1] <= data):
+                if(self.minStk[-1] >= data):
                     self.minStk.append(self.minStk[-1])
-                else:
-                    self.minStk.append(data)
             self.stk.append(data)
-            self.top+=1
+            self.brim+=1
 
-    
+    def top(self): return self.stk[-1]
+    '''
     def pop(self):
         if(self.isEmpty()):
             raise Exception("Cannot pop")
         else:
             self.minStk.pop(-1)
-            self.top-=1
+            self.brim-=1
+            return self.stk.pop(-1)
+    '''
+
+    def pop(self):
+        if(self.isEmpty()):
+            raise Exception("Cannot pop empty stack")
+        else:
+            if(self.stk[-1] == self.minStk[-1]):
+                self.minStk.pop(-1)
+            self.brim-=1
             return self.stk.pop(-1)
 
     def FindMin(self):
